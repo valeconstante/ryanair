5e26e76c6c164f12d90a6c79bcdec46f30411ac1
valeconstante <85357172+valeconstante@users.noreply.github.com>
2021-11-20 15:43:09 +0100

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#desplegable").click(function(desplegable){
+        $(".nav-principal").toggle();
+    });
+    $("#doblefuncion").click(function(doblefuncion){
+        $(".doblefuncionmenu").toggle();
+    });
+});
